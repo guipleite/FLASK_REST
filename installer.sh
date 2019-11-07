9c93711c9dfcd2dcdf0567ca0432ebaafa5a889d
guipleite <guipleite@gmail.com>
2019-11-07 14:27:08 -0300

    Aps3

---
+++ installer.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+
+sudo apt install python3
+
+sudo apt install python3-pip -y
+
+export LC_ALL=C
+
+pip3 install --upgrade pip
+
+pip3 install Flask
+
+pip3 install flask_restful
+

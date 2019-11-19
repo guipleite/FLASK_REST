d8b56c8e82f1ca83cc14f1a2dd6bc199990ae488
guipleite <guipleite@gmail.com>
2019-11-19 10:45:18 -0300

    fowarder host

--- fowarder.py
+++ fowarder.py
@@ -82,4 +82,4 @@ class TaskAPI(Resource):
 api.add_resource(TaskAPI, '/todo/api/tasks/<int:id>', endpoint = 'task')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0,debug=True)
+    app.run(host="0.0.0.0",debug=True)
